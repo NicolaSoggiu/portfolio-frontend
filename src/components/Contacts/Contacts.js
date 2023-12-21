21128b976bc31bd50a9a26ab0de95052a7b96678
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-12-21 16:13:00 +0100

    add backgroun style

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -3,29 +3,31 @@ import "./Contacts.css";
 
 const Contacts = () => {
   return (
-    <div className="card">
-      <p>
-        <a href="https://www.linkedin.com/in/nicola-soggiu-2860ba285/">
-          <span className="rotate">Linkedin</span>
-        </a>
-      </p>
-      <p>
-        <a href="https://github.com/NicolaSoggiu">
-          <span className="rotate">Github</span>
-        </a>
-      </p>
-      <p>
-        <a href="#">
-          <span className="rotate">Phone </span>
-          <div className="contact-info">3515733336</div>
-        </a>
-      </p>
-      <p>
-        <a href="#">
-          <span className="rotate">Email</span>
-          <span className="contact-info">nicola.soggiu.work@gmail.com</span>
-        </a>
-      </p>
+    <div className="containerC">
+      <div className="card">
+        <p>
+          <a href="https://www.linkedin.com/in/nicola-soggiu-2860ba285/">
+            <span className="rotate">Linkedin</span>
+          </a>
+        </p>
+        <p>
+          <a href="https://github.com/NicolaSoggiu">
+            <span className="rotate">Github</span>
+          </a>
+        </p>
+        <p>
+          <a href="#">
+            <span className="rotate">Phone </span>
+            <div className="contact-info">3515733336</div>
+          </a>
+        </p>
+        <p>
+          <a href="#">
+            <span className="rotate">Email</span>
+            <span className="contact-info">nicola.soggiu.work@gmail.com</span>
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
